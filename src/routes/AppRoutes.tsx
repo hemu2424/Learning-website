6ab796747e6e7fc5d2998c9981c0e610f8bc81e4
sandhya <sandhyadhan02@gmail.com>
2026-03-27 14:13:05 +0530

    programs routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,6 +3,7 @@ import Home from "../pages/Home";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
 import NotFound from "../pages/NotFound";
+import Programs from "../components/Programs"
 
 const AppRoutes = () => {
   return (
@@ -11,7 +12,7 @@ const AppRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        
+         <Route path="/programs" element={<Programs />} />
         {/* 404 page */}
         <Route path="*" element={<NotFound />} />
       </Routes>
